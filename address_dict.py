947e44a72d31f36cf6c0f82383d3ac63a510646e
EduardoFilhoTec <88408948+EduardoFilhoTec@users.noreply.github.com>
2021-08-10 15:53:36 -0300

    Update address_dict.py

--- address_dict.py
+++ address_dict.py
@@ -4,42 +4,14 @@ class Address_dict():
         self.nome = nome
         """
         EDITE AQUI SUA LISTA DE CONCENTRADORES, SEGUINDO O PADRÃO DA TUPLA ABAIXO
+        Nome tudo minúsculo e junto.
+        IPV4 normal.
+        ('nomeroteador', 'ip.do.seu.roteador')
         """
         self.address_list = [
-            ('aldeota1', '172.17.0.92'), ('aldeotai', '172.17.0.92'),
-            
-            ('aldeota2', '172.17.0.137'), ('aldeotaii', '172.17.0.137'),
-            
-            ('aquiraz', '172.17.0.125'), ('aquir', '172.17.0.125'),
-            
-            ('coacu', '172.17.0.97'), ('coaçu', '172.17.0.97'),
-
-            ('damas', '172.17.0.65'),
-            
-            ('diasmacedo', '172.17.0.49'), ('dias', '172.17.0.49'), ('macedo', '172.17.0.49'),
-
-            ('esperanca', '172.17.0.87'), ('esperança', '172.17.0.87'),
-
-            ('guararapes', '172.17.0.11'), ('guarara', '172.17.0.11'), ('guararape', '172.17.0.11'),
-
-            ('messejana1', '172.17.0.12'), ('messejanai', '172.17.0.12'), ('josedealencar1', '172.17.0.12'),
-
-            ('messejana2', '172.17.0.96'), ('messejanaii', '172.17.0.96'),
-
-            ('messejanashell', '172.17.0.134'), ('messejananovo', '172.17.0.134'), ('postoms', '172.17.0.134'),
-            
-            ('passare', '172.17.0.8'), ('passaré', '172.17.0.8'),
-            
-            ('pedrabranca', '172.17.0.10'), ('pedra', '172.17.0.10'),
-
-            ('portodunas', '172.17.0.25'),
-            ('portodasdunas', '172.17.0.25'), ('portaldunas', '172.17.0.25'), 
-            ('portaldasdunas', '172.17.0.25'),
-
-            ('saogerardo', '172.17.0.105'),
-
-            ('viasul', '172.20.3.2')
-
+            ('roteador1', '172.17.0.92'), ('ccr3600', '172.17.0.92'),
+            ('meuroteador', '192.168.0.10'), ('ccr1072', '192.168.0.10'),
+           
             ] # FIM DA TUPLA
     
     # Verifica se entrada 'nome' está na lista de endereços
